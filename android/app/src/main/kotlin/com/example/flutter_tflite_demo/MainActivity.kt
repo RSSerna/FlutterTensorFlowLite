20e438f83c20c0e875c42b529cafd4b38bb1d0df
RicardoSaldarriagaS <=>
2025-08-22 12:30:52 -0500

    Get Battery Android

--- android/app/src/main/kotlin/com/example/flutter_tflite_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_tflite_demo/MainActivity.kt
@@ -1,5 +1,34 @@
+import android.os.bundle
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+
 package com.example.flutter_tflite_demo
 
-import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.native_communicator"
+
+    override fun OnCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        MethodChannel(flutterEngine?.dartExecutor, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getBatteryLevel") {
+                val batteryLevel = getBatterryLevel()
+
+                if (batteryLevel != -1) {
+                    result.success("Battery Level: $batteryLevel%")
+                } else {
+                    result.error("UNAVAILABLE", "Battery level not available.", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getBatterryLevel(): Int {
+        val batteryManager = getSystemService(BATTERY_SERVICE) as android.os.BatteryManager
+        return batteryManager.getIntProperty(android.os.BatteryManager.BATTERY_PROPERTY_CAPACITY)  
+    }
+
+}
